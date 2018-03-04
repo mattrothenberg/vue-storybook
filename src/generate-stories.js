0eff61a450e96aa18de81403defb4d00c65a59f2
Matt Rothenberg <mattrothenberg@gmail.com>
2018-03-04 17:23:07 -0500

    serialize and eval methods

--- src/generate-stories.js
+++ src/generate-stories.js
@@ -1,11 +1,12 @@
-module.exports = function(group, vueComponent) {
+module.exports = function(group, vueComponent, action) {
   if (!vueComponent.__stories) return
   vueComponent.__stories.forEach(story => {
     group.add(story.name, () => ({
       components: {
         [vueComponent.name]: vueComponent
       },
-      template: story.template
+      template: story.template,
+      methods: eval(`(${story.methods})`)
     }))
   })
 }
